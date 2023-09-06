723690348df0734debd94b0be551abe350162566
Cleberson Henrique <clebersonh@yahoo.com.br>
2023-09-06 00:34:23 -0300

    refactor: structure identation

--- internal/pkg/model/models.go
+++ internal/pkg/model/models.go
@@ -1,4 +1,8 @@
 package model
 
-// Tables register table models to migrate
-var Tables = []interface{}{&User{}, &ChatRoom{}, &Chat{}}
+// Tables ...
+var Tables = []interface{}{
+	&User{},
+	&ChatRoom{},
+	&Chat{},
+}
